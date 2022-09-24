fbe43080dce522ea0570b2fea1495532dca84c05
AlexanderTemp <aaalex2025@gmail.com>
2022-09-23 23:05:18 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ npm run build
 cd dist
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:AlexanderTemp/ReactCSSTransitionsExamples.git main:gh-pages
+git push -f git@github.com:AlexanderTemp/ReactCSSTransitionsExamples.git master:gh-pages
 cd -
